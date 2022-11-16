2cb67697ad35f18c73ff75bd75afc94b2feda00a
Muhammad Haseeb <muhammmadhaseebisb@gmail.com>
2022-11-16 11:54:38 +0500

    Object Marshaling moved to Core lib helper functions

--- testHelper/bodyMatchers.go
+++ testHelper/bodyMatchers.go
@@ -2,58 +2,59 @@ package testHelper
 
 import (
 	"encoding/json"
-	"fmt"
 	"reflect"
-	"strings"
 	"testing"
 
 	"github.com/apimatic/go-core-runtime/https"
 )
 
-func NativeBodyMatcher(test *testing.T, responseBody, expectedBody string) {
+func NativeBodyMatcher(test *testing.T, expectedBody string, responseObject any) {
+	responseBytes,_ := json.Marshal(responseObject)
 	var expected, response interface{}
 	expectedError := json.Unmarshal([]byte(expectedBody), &expected)
-	responseError := json.Unmarshal([]byte(responseBody), &response)
+	responseError := json.Unmarshal(responseBytes, &response)
 
 	if expectedError != nil || responseError != nil {
 		test.Error("Error while Unmarshalling")
 	}
 
 	if !reflect.DeepEqual(response, expected) {
-		test.Errorf("got \n%v \nbut expected \n%v", responseBody, expectedBody)
+		test.Errorf("got \n%v \nbut expected \n%v", string(responseBytes), expectedBody)
 	}
 }
 
-func KeysBodyMatcher(test *testing.T, responseBody, expectedBody string, checkArrayCount, checkArrayOrder bool) {
+func KeysBodyMatcher(test *testing.T, expectedBody string, responseObject any, checkArrayCount, checkArrayOrder bool) {
 
+	responseBytes,_ := json.Marshal(responseObject)
 	var response, expected map[string]interface{}
-	responseErr := json.Unmarshal([]byte(responseBody), &response)
+	responseErr := json.Unmarshal(responseBytes, &response)
 	expectedErr := json.Unmarshal([]byte(expectedBody), &expected)
 
 	if responseErr != nil && expectedErr != nil{
 		test.Error("Error while Unmarshalling")
 	}
 
-	if !compareKeysBody(response, expected, checkArrayCount, checkArrayOrder, false) {
-		test.Errorf("got \n%v \nbut expected \n%v", responseBody, expectedBody)
+	if !matchKeysAndValuesBody(response, expected, checkArrayCount, checkArrayOrder, false) {
+		test.Errorf("got \n%v \nbut expected \n%v", string(responseBytes), expectedBody)
 	}
 }
 
-func KeysAndValuesBodyMatcher(test *testing.T, responseBody, expectedBody string, checkArrayCount, checkArrayOrder bool) {
+func KeysAndValuesBodyMatcher(test *testing.T, expectedBody string, responseObject any, checkArrayCount, checkArrayOrder bool) {
+	responseBytes,_ := json.Marshal(responseObject)
 	var response, expected map[string]interface{}
-	responseErr := json.Unmarshal([]byte(responseBody), &response)
+	responseErr := json.Unmarshal(responseBytes, &response)
 	expectedErr := json.Unmarshal([]byte(expectedBody), &expected)
 
 	if responseErr != nil && expectedErr != nil{
 		test.Error("Error while Unmarshalling")
 	}
 
-	if !compareKeysBody(response, expected, checkArrayCount, checkArrayOrder, true) {
-		test.Errorf("got \n%v \nbut expected \n%v", responseBody, expectedBody)
+	if !matchKeysAndValuesBody(response, expected, checkArrayCount, checkArrayOrder, true) {
+		test.Errorf("got \n%v \nbut expected \n%v", string(responseBytes), expectedBody)
 	}
 }
 
-func compareKeysBody(response, expected map[string]interface{}, checkArrayCount, checkArrayOrder, checkValues bool) bool {
+func matchKeysAndValuesBody(response, expected map[string]interface{}, checkArrayCount, checkArrayOrder, checkValues bool) bool {
 	if checkArrayCount && len(expected) != len(response) {
 		return false
 	}
@@ -68,7 +69,7 @@ func compareKeysBody(response, expected map[string]interface{}, checkArrayCount,
 			}
 			responseSubMap := responseValue.(map[string]interface{})
 			expectedSubMap := value.(map[string]interface{})
-			if !compareKeysBody(responseSubMap, expectedSubMap, checkArrayCount, checkArrayOrder, checkValues) {
+			if !matchKeysAndValuesBody(responseSubMap, expectedSubMap, checkArrayCount, checkArrayOrder, checkValues) {
 				return false
 			}
 		} else if checkValues && !reflect.DeepEqual(responseValue, value) {
@@ -78,7 +79,10 @@ func compareKeysBody(response, expected map[string]interface{}, checkArrayCount,
 	return true
 }
 
-func RawBodyMatcher(test *testing.T, responseBody, expectedBody string) {
+func RawBodyMatcher(test *testing.T, expectedBody string, responseObject any) {
+	responseBytes,_ := json.Marshal(responseObject)
+	responseBody := string(responseBytes)
+
 	if responseBody != expectedBody {
 		test.Errorf("got \n%v \nbut expected %v", responseBody, expectedBody)
 	}
@@ -89,27 +93,4 @@ func IsSameFile(test *testing.T, expectedFileURL string, responseFile https.File
 	if !reflect.DeepEqual(responseFile, expectedFile) {
 		test.Error("Response File does not match the File received")
 	}
-}
-
-func SliceToCommaSeparatedString(slice interface{}) string {
-	return strings.Join(strings.Split(fmt.Sprint(slice), " "), ",")
-}
-
-func toStringSlice(input []reflect.Value) []string {
-	stringSlice := make([]string, 0)
-	for _, v := range input {
-		stringSlice = append(stringSlice, v.Interface().(string))
-	}
-	return stringSlice
-}
-
-func getMapKeys(input map[string]interface{}) []reflect.Value {
-	keys := reflect.ValueOf(input).MapKeys()
-	for _, v := range input {
-		x := reflect.ValueOf(v)
-		if x.Kind() == reflect.Map {
-			keys = append(keys, getMapKeys(v.(map[string]interface{}))...)
-		}
-	}
-	return keys
 }
