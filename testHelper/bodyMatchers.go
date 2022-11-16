162bd8191c09152361b3edd9a6180a16c4eede05
Muhammad Haseeb <109739087+haseeb-apimatic@users.noreply.github.com>
2022-11-16 12:31:06 +0500

    feat(go): add testHelper Package for Unit Tests response Matching (#2)

---
+++ testHelper/bodyMatchers.go
@@ -0,0 +1,101 @@
+package testHelper
+
+import (
+	"encoding/json"
+	"fmt"
+	"reflect"
+	"strings"
+	"testing"
+
+	"github.com/apimatic/go-core-runtime/https"
+)
+
+func NativeBodyMatcher(test *testing.T, expectedBody string, responseObject any) {
+	responseBytes,_ := json.Marshal(responseObject)
+	var expected, response interface{}
+	expectedError := json.Unmarshal([]byte(expectedBody), &expected)
+	responseError := json.Unmarshal(responseBytes, &response)
+
+	if expectedError != nil || responseError != nil {
+		test.Error("Error while Unmarshalling")
+	}
+
+	if !reflect.DeepEqual(response, expected) {
+		test.Errorf("got \n%v \nbut expected \n%v", string(responseBytes), expectedBody)
+	}
+}
+
+func KeysBodyMatcher(test *testing.T, expectedBody string, responseObject any, checkArrayCount, checkArrayOrder bool) {
+	responseBytes,_ := json.Marshal(responseObject)
+	var response, expected map[string]interface{}
+	responseErr := json.Unmarshal(responseBytes, &response)
+	expectedErr := json.Unmarshal([]byte(expectedBody), &expected)
+
+	if responseErr != nil && expectedErr != nil {
+		test.Error("Error while Unmarshalling")
+	}
+
+	if !matchKeysAndValues(response, expected, checkArrayCount, checkArrayOrder, false) {
+		test.Errorf("got \n%v \nbut expected \n%v", string(responseBytes), expectedBody)
+	}
+}
+
+func KeysAndValuesBodyMatcher(test *testing.T, expectedBody string, responseObject any, checkArrayCount, checkArrayOrder bool) {
+	responseBytes,_ := json.Marshal(responseObject)
+	var response, expected map[string]interface{}
+	responseErr := json.Unmarshal(responseBytes, &response)
+	expectedErr := json.Unmarshal([]byte(expectedBody), &expected)
+
+	if responseErr != nil && expectedErr != nil {
+		test.Error("Error while Unmarshalling")
+	}
+
+	if !matchKeysAndValues(response, expected, checkArrayCount, checkArrayOrder, true) {
+		test.Errorf("got \n%v \nbut expected \n%v", string(responseBytes), expectedBody)
+	}
+}
+
+func matchKeysAndValues(response, expected map[string]interface{}, checkArrayCount, checkArrayOrder, checkValues bool) bool {
+	if checkArrayCount && len(expected) != len(response) {
+		return false
+	}
+	for key, value := range expected {
+		responseValue := response[key]
+		if responseValue == nil {
+			return false
+		}
+		if reflect.ValueOf(responseValue).Kind() == reflect.Map {
+			if reflect.ValueOf(value).Kind() != reflect.Map {
+				return false
+			}
+			responseSubMap := responseValue.(map[string]interface{})
+			expectedSubMap := value.(map[string]interface{})
+			if !matchKeysAndValues(responseSubMap, expectedSubMap, checkArrayCount, checkArrayOrder, checkValues) {
+				return false
+			}
+		} else if checkValues && !reflect.DeepEqual(responseValue, value) {
+			return false
+		}
+	}
+	return true
+}
+
+func RawBodyMatcher(test *testing.T, expectedBody string, responseObject any) {
+	responseBytes,_ := json.Marshal(responseObject)
+	responseBody := string(responseBytes)
+
+	if responseBody != expectedBody {
+		test.Errorf("got \n%v \nbut expected %v", responseBody, expectedBody)
+	}
+}
+
+func IsSameFile(test *testing.T, expectedFileURL string, responseFile https.FileWrapper) {
+	expectedFile := https.GetFile(expectedFileURL).File
+	if !reflect.DeepEqual(responseFile, expectedFile) {
+		test.Error("Response File does not match the File received")
+	}
+}
+
+func SliceToCommaSeparatedString(slice interface{}) string {
+	return strings.Join(strings.Split(fmt.Sprint(slice), " "), ",")
+}
---
+++ testHelper/headersMatchers.go
@@ -0,0 +1,37 @@
+package testHelper
+
+import (
+	"net/http"
+	"testing"
+)
+
+type TestHeader struct {
+	CheckValue bool   `json:"CheckValue"`
+	Name       string `json:"Name"`
+	Value      string `json:"Value"`
+}
+
+func NewTestHeader(checkValue bool, name, value string) TestHeader {
+	return TestHeader{
+		CheckValue: checkValue,
+		Name:       name,
+		Value:      value,
+	}
+}
+
+func CheckResponseHeaders(t *testing.T, responseHeaders http.Header, expectedHeadersList []TestHeader, allowExtraHeaders bool) {
+	for _, expectedHeader := range expectedHeadersList {
+		respValue := responseHeaders.Get(expectedHeader.Name)
+		if respValue == "" {
+			t.Errorf("expected header '%v' does not exists in response", expectedHeader.Name)
+			break
+		} else if expectedHeader.CheckValue && respValue != expectedHeader.Value {
+			t.Errorf("response does not contains same value of expected header '%v'", expectedHeader.Name)
+			break
+		}
+	}
+
+	if !allowExtraHeaders && len(responseHeaders) != len(expectedHeadersList) {
+		t.Errorf("response contains other headers than those listed in the expected headers list")
+	}
+}
