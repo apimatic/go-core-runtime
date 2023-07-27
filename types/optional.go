10a04f8f68c389bdd8cee6df5901c25902dcaf44
Bisma <bisma.pervaiz@apimatic.io>
2023-07-27 12:02:53 +0500

    Adding types docs

--- types/optional.go
+++ types/optional.go
@@ -1,14 +1,17 @@
+// Package types provides utility types and functions.
+// Copyright (c) APIMatic. All rights reserved.
 package types
 
 import "encoding/json"
 
-// Optional struct leverages any type as optional and nullable.
+// Optional is a generic struct that allows any type to be used as optional and nullable.
 // Optional.set is true when Optional.value is to be used.
 type Optional[T any] struct {
 	value *T
 	set   bool // set is true when its value is to be used
 }
 
+// NewOptional creates and returns an Optional instance with the given value set.
 func NewOptional[T any](value *T) Optional[T] {
 	return Optional[T]{
 		value: value,
@@ -16,6 +19,7 @@ func NewOptional[T any](value *T) Optional[T] {
 	}
 }
 
+// EmptyOptional creates and returns an empty Optional instance with no value set.
 func EmptyOptional() Optional[any] {
 	return Optional[any]{
 		value: nil,
@@ -23,22 +27,27 @@ func EmptyOptional() Optional[any] {
 	}
 }
 
+// Value returns the value stored in the Optional. It returns nil if no value is set.
 func (o *Optional[T]) Value() *T {
 	return o.value
 }
 
+// SetValue sets the value of the Optional.
 func (o *Optional[T]) SetValue(value *T) {
 	o.value = value
 }
 
+// ShouldSetValue sets whether the value should be used or not.
 func (o *Optional[T]) ShouldSetValue(set bool) {
 	o.set = set
 }
 
+// IsValueSet returns true if a value is set in the Optional, false otherwise.
 func (o *Optional[T]) IsValueSet() bool {
 	return o.set
 }
 
+// UnmarshalJSON unmarshals the JSON input into the Optional value.
 func (o *Optional[T]) UnmarshalJSON(input []byte) error {
 	var temp *T
 	if input != nil {
