b05921948da74cdf1bbcaf11b86c0a0fbf3dcbda
mrafnadeem-apimatic <muhammad.nadeem@apimatic.io>
2024-02-07 13:21:16 +0500

    simplify mocking server

--- https/mockingServer.go
+++ https/mockingServer.go
@@ -12,21 +12,14 @@ func GetTestingServer() *httptest.Server {
 		case "GET":
 			switch r.URL.Path {
 			case "/response/integer":
-				w.WriteHeader(http.StatusOK)
 				w.Write([]byte(`4`))
-			case "/template/abc/def", "/template/1/2/3/4/5":
-				w.WriteHeader(http.StatusOK)
-				w.Write([]byte(`"passed": true,
-				"message": "It's a hit!",`))
-			case "/response/binary":
-				w.WriteHeader(http.StatusOK)
+			case "/template/abc/def", "/template/1/2/3/4/5", "/response/binary":
 				w.Write([]byte(`"passed": true,
 				"message": "It's a hit!",`))
 			}
 		case "POST":
 			switch r.URL.Path {
 			case "/form/string":
-				w.WriteHeader(http.StatusOK)
 				w.Write([]byte(`4`))
 			}
 		}
