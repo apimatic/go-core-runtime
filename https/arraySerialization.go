11bdb63b157bd9df71e8db8e3a1a2f28285a76e0
Mujahid Daud Khan <mujahiddaudkhan@gmail.com>
2024-02-29 11:55:50 +0500

    refactor: rename func names

--- https/arraySerialization.go
+++ https/arraySerialization.go
@@ -28,26 +28,9 @@ func (option ArraySerializationOption) getSeparator() rune {
 	}
 }
 
-func (option ArraySerializationOption) appendValue(result map[string][]string, key string, value string) {
-	separator := option.getSeparator()
-	if len(result[key]) > 0 && separator != -1 {
-		result[key][0] = fmt.Sprintf("%v%c%v", result[key][0], separator, value)
-	} else {
-		result[key] = append(result[key], value)
-	}
-}
 
-func (option ArraySerializationOption) appendMap(result map[string][]string, param map[string][]string) {
-	for k, v := range param {
-		for _, v1 := range v {
-			option.appendValue(result, k, v1)
-		}
-	}
-}
-
-func (option ArraySerializationOption) prePareKey(keyPrefix string, obj any) string {
-
-	if (obj == nil) {
+func (option ArraySerializationOption) joinKey(keyPrefix string, index any) string {
+	if (index == nil) {
 		switch option {
 		case UnIndexed:
 			return fmt.Sprintf("%v[]", keyPrefix)
@@ -56,6 +39,26 @@ func (option ArraySerializationOption) prePareKey(keyPrefix string, obj any) str
 		}
 		return fmt.Sprintf("%v", keyPrefix)
 	}
-	indexedKey := fmt.Sprintf("%v", obj)
+	indexedKey := fmt.Sprintf("%v", index)
 	return fmt.Sprintf("%v[%v]", keyPrefix, indexedKey)
 }
+
+
+func (option ArraySerializationOption) appendMap(result map[string][]string, param map[string][]string) {
+	for k, v := range param {
+		for _, v1 := range v {
+			option.append(result, k, v1)
+		}
+	}
+}
+
+func (option ArraySerializationOption) append(result map[string][]string, key string, value string) {
+	separator := option.getSeparator()
+	if len(result[key]) > 0 && separator != -1 {
+		result[key][0] = fmt.Sprintf("%v%c%v", result[key][0], separator, value)
+	} else {
+		result[key] = append(result[key], value)
+	}
+}
+
+
